8d7be5cabeccd868ac9359d5a40d9bc833f9cff6
ammar.rifai <startalex123@gmail>
2025-04-13 22:05:00 +0200

    fixing typo

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "terraform_app_server" {
+  ami           = "ami-03250b0e01c28d196"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test-terraform_app_server
+  }
+}
+
