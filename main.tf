057db548c396ec1bb3c2a4751dc9201e730b7227
ammar.rifai <startalex123@gmail.com>
2025-04-17 18:33:16 +0200

    adding .gitignore file

--- main.tf
+++ main.tf
@@ -10,15 +10,28 @@ terraform {
 }
 
 provider "aws" {
+<<<<<<< HEAD
   region  = "eu-central-1"
 }
 
 resource "aws_instance" "terraform_app_server" {
+=======
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+>>>>>>> 19c9b5e (adding .gitignore file)
   ami           = "ami-03250b0e01c28d196"
   instance_type = "t2.micro"
 
   tags = {
+<<<<<<< HEAD
     Name = "test-terraform_app_server"
   }
 }
 
+=======
+    Name = "terraform-app-server"
+  }
+}
+>>>>>>> 19c9b5e (adding .gitignore file)
